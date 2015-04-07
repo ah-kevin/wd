76e73b107359340948fcc652173e75efa9311885
lennon <kbjwdh@gmail.com>
2015-04-07 17:02:33 +0800

    4.7修改样式

--- js/taocan_detail.js
+++ js/taocan_detail.js
@@ -1,35 +1,47 @@
 $(function(){
 	bodyWidth=$("body").width();
-	taocan_proWidth=bodyWidth/3.5;
+	taocan_proWidth=bodyWidth/3.5;//3.5代表一个屏幕显示3.5个
+	taocanDetailWidth=bodyWidth-20;//一个屏幕显示一个，减20因为padding
 	init();	
 
 	$(".taocan_pro").on("click",function(){
 		var cla=$(this).attr("class"),
+			width=$(".taocan_pro").width()+10,
 			num=$('.taocan_list .taocan_pro').index(this);
-			left=(num)*bodyWidth;		
-		$(".taocan_pro").css("left","-"+left/3+"px");
+			left=(num)*(bodyWidth-2);
+		$(".taocan_pro").css("left","-"+width*num+"px");
+		$(".taocan_info").css("left","-"+(bodyWidth+2)*num+"px");	
         $(this).addClass('chosce').siblings('.taocan_pro').removeClass('chosce');
 	});
 
-	$(".taocan_pro").on("swipeLeft",function(){
-		var imgs=$(".taocan_pro"),
-			divs=$(".taocan_pro"),
+	$(".taocan_info").on("swipeLeft",function(){
+		var divs=$(".taocan_pro"),
 			div=divs.eq(0),
-			left=parseInt(div.css("left"));
+			left=parseInt(div.css("left")),
+			taocanDivs=$(".taocan_info"),
+			taocanDiv=taocanDivs.eq(0),
+			num=$(".taocan_info").index(this)+2,
+			taocanLeft=parseInt(taocanDiv.css("left"));
 			if(left>-(divs.length*taocan_proWidth)){
-				imgs.css("left",(left-taocan_proWidth-10)+"px");
+				divs.css("left",(left-taocan_proWidth-12)+"px");
+				taocanDivs.css("left",(taocanLeft-taocanDetailWidth-22)+"px");
+				$(".img"+num+"").addClass('chosce').siblings('.taocan_pro').removeClass('chosce');
 			}
-		$(this).addClass('chosce').siblings('.taocan_pro').removeClass('chosce');
+
 	});
-	$(".taocan_pro").on("swipeRight",function(){
-		var imgs=$(".taocan_pro"),
-			divs=$(".taocan_pro"),
+	$(".taocan_info").on("swipeRight",function(){
+		var divs=$(".taocan_pro"),
 			div=divs.eq(0),
-			left=parseInt(div.css("left"));
-		if(left<10){
-			imgs.css("left",(left+taocan_proWidth+10)+"px");		
+			left=parseInt(div.css("left")),
+			taocanDivs=$(".taocan_info"),
+			taocanDiv=taocanDivs.eq(0),
+			num=$(".taocan_info").index(this),
+			taocanLeft=parseInt(taocanDiv.css("left"));
+		if(left<0){
+			divs.css("left",(left+taocan_proWidth+12)+"px");
+			taocanDivs.css("left",(taocanLeft+taocanDetailWidth+22)+"px");	
+			$(".img"+num+"").addClass('chosce').siblings('.taocan_pro').removeClass('chosce');
 		}
-		$(this).addClass('chosce').siblings('.taocan_pro').removeClass('chosce');
 	});
 
 
@@ -40,5 +52,6 @@ $(function(){
 
 
 function init(){
-	$(".taocan_pro").css("width",taocan_proWidth+"px")
+	$(".taocan_pro").css("width",taocan_proWidth+"px");
+	$(".taocan_info").css("width",taocanDetailWidth+"px");
 }
